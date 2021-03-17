5789e95c25d6a8821ff3b6eb50e6ff06aaa6bafc
rylieohashi <xxryliex@gmail.com>
2021-03-17 18:32:49 -0400

    built first functional component

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>What's your name?</h1>
+            <p>Enter your name in the box below.</p>
+        </div>
+    )
+}
+
+export default Header;
