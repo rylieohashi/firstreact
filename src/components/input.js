6353a6c7a628a6441a5b8eaee7df385811133717
rylieohashi <xxryliex@gmail.com>
2021-03-17 18:44:09 -0400

    built first class component for input container and built an input functional component to render custom inputs

---
+++ src/components/input.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Input = () => {
+    return (
+        <div className="input">
+            <input />
+            <label>Your Name Here</label>
+        </div>
+    )
+}
+
+export default Input;
