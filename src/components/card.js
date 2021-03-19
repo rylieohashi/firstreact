31e7a9223eafe004fd18c7a649a468d375f8a53e
rylieohashi <xxryliex@gmail.com>
2021-03-18 21:46:39 -0400

    added initial state and event handlers

--- src/components/card.js
+++ src/components/card.js
@@ -2,12 +2,55 @@ import React, { Component } from 'react';
 
 import Input from './input';
 
+const INITIAL_STATE = {
+    yourName: '',
+    contentVisible: false
+}
+
 class Card extends Component {
+
+    constructor() {
+        super()
+            this.state = INITIAL_STATE;
+
+            this.handleInputChange = this.handleInputChange.bind(this);
+            this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    }
+
+    handleInputChange(event) {
+        this.setState({[event.target.name]: event.target.value})
+    }
+
+    handleFormSubmit(event) {
+        event.preventDefault()
+
+        if(this.state.contentVisible) {
+            this.setState (INITIAL_STATE)
+        } else {
+            this.setState({ contentVisible: true})
+        }
+    }
+
     render() {
+
+        const inputData = [
+            {title: 'Your Name', state: this.state.yourName, name: 'yourName'}
+        ]
+
         return (
-            <div className="card">
-                { Input() }
-            </div>
+            <form onSubmit={this.handleFormSubmit} className="card">
+                <div className="card__inputs">
+                { 
+                    inputData.map((data) => {
+                        return Input( (data), this.handleInputChange)
+                    })
+                }
+                </div>
+                <button className={`card__${!this.state.contentVisible ? 'generate' : 'clear'}`} type="submit">{!this.state.contentVisible ? 'Who are you' : 'Good Bye'}</button>
+                {
+                    //this.state.contentVisible ? <Content data={this.state}/> 
+                }
+        </form>
         )
     }
 }
